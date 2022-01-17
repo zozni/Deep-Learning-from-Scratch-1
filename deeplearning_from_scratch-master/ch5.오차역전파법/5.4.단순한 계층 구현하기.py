b041fb404e4f4b7a0592f37c1856b866df6f5e48
zo_zni <thekey1027@naver.com>
2022-01-17 17:44:51 +0900

    6

--- deeplearning_from_scratch-master/ch5.오차역전파법/5.4.단순한 계층 구현하기.py
+++ deeplearning_from_scratch-master/ch5.오차역전파법/5.4.단순한 계층 구현하기.py
@@ -1,35 +1,36 @@
 # 5.4.1 곱셈 계층
 class MulLayer:
-    def __init__(self):
-        self.x = None
+    def __init__(self):  # 인스턴스 변수인 x와 y를 초기화한다.
+        self.x = None    # 이 두 변수는 순전파 시의 입력값을 유지하기 위해 사용한다.
         self.y = None
 
-    def forward(self, x, y):
-        self.x = x
+    def forward(self, x, y):   # x와 y를 인수로 받고 두 값을 곱해서 반환한다.
+        self.x = x 
         self.y = y
         out = x * y
         return out
 
-    def backward(self, dout):
-        dx = dout * self.y
+    def backward(self, dout):   # 상류에서 넘어온 미분에 순전파때의 값을 서로 바꿔 곱한 후 하류로 흘린다.
+        dx = dout * self.y  # x와 y를 바꾼다. 
         dy = dout * self.x
         return dx, dy
 
 
 # 5.4.2 덧셈 계층
 class AddLayer:
-    def __init__(self):
+    def __init__(self):  # 초기화가 필요없으니 아무일도 하지 않는다.
         pass
 
-    def forward(self, x, y):
+    def forward(self, x, y):  # 입력받은 두 인수 x, y를 더해서 반환한다.
         out = x + y
         return out
 
-    def backward(self, dout):
+    def backward(self, dout):  # 상류에서 내려온 미분을 그대로 하류로 흘린다.
         dx = dout * 1
         dy = dout * 1
         return dx, dy
 
+""" buy_apple.py """
 
 if __name__ == '__main__':
     # 문제1의 예시
@@ -47,12 +48,15 @@ if __name__ == '__main__':
 
     print(price)  # 220.0
 
-    # 역전파
+    # 역전파          # 각 변수에 대한 미분은 backward()에서 구할 수 있다.
     dprice = 1
     dapple_price, dtax = mul_tax_layer.backward(dprice)
     dapple, dapple_num = mul_apple_layer.backward(dapple_price)
 
     print(dapple, dapple_num, dtax)  # 2.2 110.0 200
+    
+    """ backward() 호출 순서는 forward() 때와 반대이다. """
+    """ 또 backward()가 받는 인수는 순전파의 출력에 대한 미분이다."""
 
     # 문제2의 예시
     orange = 150
@@ -81,3 +85,6 @@ if __name__ == '__main__':
 
     print(dapple_num, dapple, dornage, dorange_num, dtax)
     # 110.0 2.2 3.3 165.0 650
+
+""" 필요한 계층을 만들어 forward()를 적절한 순서로 호출한다. 
+    그런 다음 순전파와 반대 순서로 역전파 메소드인 backward()를 호출하면 원하는 미분이 나온다. """
